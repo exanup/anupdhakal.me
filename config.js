2ff0d50d40882fecf0a559b1250b9292a0e49b05
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2020-11-28 21:36:17 +0545

    🚀 Initial commit

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://anupdhakal.me',
+  pathPrefix: '/',
+  title: 'Anup Dhakal',
+  subtitle: 'Personal Blog',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/',
+    },
+    {
+      label: 'About me',
+      path: '/pages/about',
+    },
+    {
+      label: 'Resume',
+      path: '/pages/resume',
+    },
+  ],
+  author: {
+    name: 'Anup Dhakal',
+    photo: '/photo.jpg',
+    bio: 'Kathmandu-based software engineer. I enjoy building things!',
+    contacts: {
+      email: 'anupdhakal2000@gmail.com',
+      telegram: 'exanup',
+      twitter: 'nup66',
+      github: 'exanup',
+      linkedin: 'exanup',
+      facebook: '',
+      rss: '',
+      vkontakte: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    },
+  },
+};
