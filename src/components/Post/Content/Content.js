27f959aaaf1f3d6d7bd263a78bdfbf7f85c3e69f
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2021-01-08 23:44:32 +0545

    Fix fonts and styles

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,16 +1,24 @@
 // @flow strict
-import React from 'react';
-import styles from './Content.module.scss';
+import React from "react";
+import styles from "./Content.module.scss";
 
 type Props = {
   body: string,
-  title: string
+  title: string,
+  description: string,
 };
 
-const Content = ({ body, title }: Props) => (
-  <div className={styles['content']}>
-    <h1 className={styles['content__title']}>{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+const Content = ({ body, title, description }: Props) => (
+  <div className={styles["content"]}>
+    <h1 className={styles["content__title"]}>{title}</h1>
+    <hr />
+    <div className={styles["content__description"]}>
+      <p>{description}</p>
+    </div>
+    <div
+      className={styles["content__body"]}
+      dangerouslySetInnerHTML={{ __html: body }}
+    />
   </div>
 );
 
