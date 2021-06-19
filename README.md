3b2685cf8b975fb1a3e77c3ce59456b92f96e840
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2021-06-19 15:12:09 +0545

    Remove npm from engines

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [anupdhakal.me](https://anupdhakal.me)
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/edf1c647-d9e3-4419-ad35-28ad41ef4f78/deploy-status)](https://app.netlify.com/sites/anupdhakal/deploys)
+
 My personal blog, shamelessly "inspired" from and based upon
 [Foundation](https://github.com/stackrole/gatsby-starter-foundation).
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,16 @@ const netlifyCmsPaths = {
 
 const settings = require('./src/util/site.json')
 
+const {
+    NODE_ENV,
+    URL: NETLIFY_SITE_URL = settings.meta.siteUrl,
+    DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+    CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
     siteMetadata: settings.meta,
     plugins: [
@@ -115,8 +125,8 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-robots-txt',
             options: {
-                host: settings.meta.siteUrl,
-                sitemap: `${settings.meta.siteUrl}/sitemap`,
+                host: siteUrl,
+                sitemap: `${siteUrl}/sitemap`,
                 resolveEnv: () => NETLIFY_ENV,
                 env: {
                     production: {
