bfad3d9b8ace04322bb5745b7d809e2ef75fd194
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2021-06-19 18:47:41 +0545

    Don't preload the web fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -206,7 +206,7 @@ module.exports = {
                     ],
                 },
                 useMinify: true,
-                usePreload: true,
+                usePreload: false,
                 usePreconnect: true,
             },
         },
